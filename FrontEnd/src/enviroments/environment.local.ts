1c42e863bb660487ac7f9213be3dac8354d70763
Дзабахідзе Давид <davyd.dzabakhidze@nure.ua>
2022-12-17 12:15:42 +0200

    front-end/update env

---
+++ FrontEnd/src/enviroments/environment.local.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://localhost:7151/api/V1/',
+  hostingUrl: 'http://localhost:4200',
+};
---
+++ FrontEnd/src/enviroments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://localhost:7151/',
+  hostingUrl: 'http://localhost:4200',
+};
